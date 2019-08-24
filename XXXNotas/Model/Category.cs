23601875c84f17e00ea07f9f6befa1606797f00c
郭跃波 <yuebo.guo@juphoon.com.cn>
2019-08-24 14:20:49 +0800

    添加model

---
+++ XXXNotas/Model/Category.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XXXNotas.Model
+{
+    /// <summary>
+    /// 目录类 需要可序列化
+    /// </summary>
+    [Serializable]
+    class Category : Notifier
+    {
+        private Guid _id;
+        private string _name;
+        private string _backgroundColor;
+        private string _fontColor;
+        private bool _isDefault;
+
+        /// <summary>
+        /// 初始化一个 <see cref="Category"/> 类
+        /// </summary>
+        /// <param name="name">目录名称</param>
+        /// <param name="backgroundColor">目录背景色</param>
+        /// <param name="fontColor">目录字体颜色</param>
+        public Category(string name, string backgroundColor, string fontColor)
+        {
+            _id = Guid.NewGuid();
+            _name = name;
+            _backgroundColor = backgroundColor;
+            _fontColor = fontColor;
+        }
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        public Guid Id
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+                if(_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 目录名称
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 背景色
+        /// </summary>
+        public string BackgroundColor
+        {
+            get
+            {
+                return _backgroundColor;
+            }
+            set
+            {
+                if(_backgroundColor != value)
+                {
+                    _backgroundColor = value;
+                    OnPropertyChanged("BackgroundColor");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 字体颜色
+        /// </summary>
+        public string FontColor
+        {
+            get
+            {
+                return _fontColor;
+            }
+            set
+            {
+                if(_fontColor != value)
+                {
+                    _fontColor = value;
+                    OnPropertyChanged("FontColor");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 是否是默认目录
+        /// </summary>
+        public bool IsDefault
+        {
+            get
+            {
+                return _isDefault;
+            }
+            set
+            {
+                if(_isDefault != value)
+                {
+                    _isDefault = value;
+                    OnPropertyChanged("IsDefault");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 表示该 <see cref="System.Object"/> 是否是本实例相同
+        /// </summary>
+        /// <param name="obj">比较的 <see cref="System.Object"/></param>
+        /// <returns><c>true</c>表示相同</returns>
+        public override bool Equals(object obj)
+        {
+            Category category = obj as Category;
+            return category != null && (category.Id == this.Id && category.Name == this.Name);
+        }
+
+        /// <summary>
+        /// 重写Equals方法后需要重写GetHashCode方法
+        /// </summary>
+        /// <returns>返回的对象哈希值</returns>
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() + Name.GetHashCode();
+        }
+    }
+}
---
+++ XXXNotas/Model/Note.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XXXNotas.Model
+{
+    /// <summary>
+    /// 笔记类 需要可序列化
+    /// </summary>
+    class Note : Notifier
+    {
+        private string _content;
+        private string _date;
+        private Category _category;
+
+        /// <summary>
+        /// 创建一个新实例 由于需要在VM中创建Note，故需要无参构造
+        /// </summary>
+        public Note() : this(string.Empty, null) { }
+
+        /// <summary>
+        /// 初始化一个新的Note实例
+        /// </summary>
+        /// <param name="content">内容</param>
+        /// <param name="category">目录</param>
+        public Note(string content, Category category)
+        {
+            _content = content;
+            _category = category;
+        }
+
+        /// <summary>
+        /// 笔记内容
+        /// </summary>
+        public string Content
+        {
+            get
+            {
+                return _content;
+            }
+            set
+            {
+                if(_content != value)
+                {
+                    _content = value;
+                    OnPropertyChanged("Content");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public string Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                if(_date != value)
+                {
+                    _date = value;
+                    OnPropertyChanged("Date");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 笔记所在目录
+        /// </summary>
+        public Category Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                if(_category != value)
+                {
+                    _category = value;
+                    OnPropertyChanged("Category");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 重写判断对象相同的方法
+        /// </summary>
+        /// <param name="obj">需要比较的对象</param>
+        /// <returns>true表示相同</returns>
+        public override bool Equals(object obj)
+        {
+            Note note = obj as Note;
+            return note == null && (note.Content == this.Content && note.Category.Equals(this));
+        }
+
+        /// <summary>
+        /// 重写Equals方法后需要重写GetHashCode方法
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Content.GetHashCode() + Category.GetHashCode();
+        }
+
+    }
+}
